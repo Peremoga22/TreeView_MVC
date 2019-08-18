74bf5ddca6dcf9d85dd5e40397ca6b9f395233ae
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-18 14:09:10 +0300

    Update TreeNodeController.cs
    Use Dependency injection and all logic writer in FileService whare  convert to json and serialization data;

--- WebAppConvertFolderDataToJasonFormat/WebAppConvertFolderDataToJasonFormat/Controllers/TreeNodeController.cs
+++ WebAppConvertFolderDataToJasonFormat/WebAppConvertFolderDataToJasonFormat/Controllers/TreeNodeController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebAppConvertFolderDataToJasonFormat.Services.Interfaces;
 
 namespace WebAppConvertFolderDataToJasonFormat.Controllers
 {
     public class TreeNodeController : Controller
     {
+        private readonly IFileService _fileService;
+
+        public TreeNodeController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string folder)
+        {
+            var res = _fileService.OutPutInfo(folder);
+            var serealizeResult = JsonConvert.SerializeObject(res, Formatting.Indented);
+            return Content(serealizeResult);
         }
     }
 }
